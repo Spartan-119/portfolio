93e883102542ec48eaa5ae99e79b570139b9c547
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2024-07-24 19:52:57 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -29310,6 +29310,8 @@ var _voPng = require("../images/vo.png");
 var _voPngDefault = parcelHelpers.interopDefault(_voPng);
 var _gnnPng = require("../images/gnn.png");
 var _gnnPngDefault = parcelHelpers.interopDefault(_gnnPng);
+var _ragPng = require("../images/rag.png");
+var _ragPngDefault = parcelHelpers.interopDefault(_ragPng);
 // const imageAltText = "desktop with books and laptop";
 const projectList = [
     {
@@ -29329,6 +29331,12 @@ const projectList = [
         description: "Designed an ATS to help streamline the recruitment process by matching job descriptions with candidate resumes. Used NLP techniques to extract keywords from resumes and job descriptions to match them and assign a matching score.",
         url: "https://github.com/Spartan-119/ats",
         thumbnail: (0, _atsJpegDefault.default)
+    },
+    {
+        title: "Ranking Job Descriptions using RAG",
+        description: "Built a simple RAG pipeline to rank the quality of job descriptions based on 25 KPIs identified. This system scores based on a semantic similarity search.",
+        url: "https://github.com/Spartan-119/rag_ranking",
+        thumbnail: (0, _ragPngDefault.default)
     },
     {
         title: "LightGlue Visual Odometry",
@@ -29418,7 +29426,7 @@ const Portfolio = ()=>{
                 children: "Portfolio Projects"
             }, void 0, false, {
                 fileName: "src/Components/Portfolio.jsx",
-                lineNumber: 141,
+                lineNumber: 149,
                 columnNumber: 7
             }, undefined),
             rows.map((row, rowIndex)=>/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
@@ -29445,20 +29453,20 @@ const Portfolio = ()=>{
                                             }
                                         }, void 0, false, {
                                             fileName: "src/Components/Portfolio.jsx",
-                                            lineNumber: 152,
+                                            lineNumber: 160,
                                             columnNumber: 17
                                         }, undefined),
                                         /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("h3", {
                                             children: project.title
                                         }, void 0, false, {
                                             fileName: "src/Components/Portfolio.jsx",
-                                            lineNumber: 157,
+                                            lineNumber: 165,
                                             columnNumber: 17
                                         }, undefined)
                                     ]
                                 }, void 0, true, {
                                     fileName: "src/Components/Portfolio.jsx",
-                                    lineNumber: 151,
+                                    lineNumber: 159,
                                     columnNumber: 15
                                 }, undefined),
                                 /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("p", {
@@ -29466,24 +29474,24 @@ const Portfolio = ()=>{
                                     children: project.description
                                 }, void 0, false, {
                                     fileName: "src/Components/Portfolio.jsx",
-                                    lineNumber: 159,
+                                    lineNumber: 167,
                                     columnNumber: 15
                                 }, undefined)
                             ]
                         }, project.title, true, {
                             fileName: "src/Components/Portfolio.jsx",
-                            lineNumber: 150,
+                            lineNumber: 158,
                             columnNumber: 13
                         }, undefined))
                 }, `row_${rowIndex}`, false, {
                     fileName: "src/Components/Portfolio.jsx",
-                    lineNumber: 144,
+                    lineNumber: 152,
                     columnNumber: 9
                 }, undefined))
         ]
     }, void 0, true, {
         fileName: "src/Components/Portfolio.jsx",
-        lineNumber: 140,
+        lineNumber: 148,
         columnNumber: 5
     }, undefined);
 };
@@ -29497,7 +29505,7 @@ $RefreshReg$(_c, "Portfolio");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","../images/a_b testing.png":"hrff5","../images/price_prediction.png":"jcEeo","../images/credit_card_fraud_detection.png":"ix340","../images/flight_ticket.png":"dSzE0","../images/fake_news.png":"eb8s5","../images/pong.png":"16zGv","../images/conway.png":"7ZeZL","../images/netflix.png":"3VHEg","../images/gan.png":"jecTi","../images/ap.png":"flU6n","../images/crime_statistics.png":"9EEg0","../images/support2.png":"dlAIR","../images/ats.jpeg":"hbc0J","../images/vo.png":"2o2r1","../images/gnn.png":"6pDKv","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"hrff5":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","../images/a_b testing.png":"hrff5","../images/price_prediction.png":"jcEeo","../images/credit_card_fraud_detection.png":"ix340","../images/flight_ticket.png":"dSzE0","../images/fake_news.png":"eb8s5","../images/pong.png":"16zGv","../images/conway.png":"7ZeZL","../images/netflix.png":"3VHEg","../images/gan.png":"jecTi","../images/ap.png":"flU6n","../images/crime_statistics.png":"9EEg0","../images/support2.png":"dlAIR","../images/ats.jpeg":"hbc0J","../images/vo.png":"2o2r1","../images/gnn.png":"6pDKv","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru","../images/rag.png":"i8izv"}],"hrff5":[function(require,module,exports) {
 module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "a_b testing.ffc291e7.png" + "?" + Date.now();
 
 },{"./helpers/bundle-url":"lgJ39"}],"jcEeo":[function(require,module,exports) {
@@ -29541,6 +29549,9 @@ module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "vo.25a
 
 },{"./helpers/bundle-url":"lgJ39"}],"6pDKv":[function(require,module,exports) {
 module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "gnn.c77c9599.png" + "?" + Date.now();
+
+},{"./helpers/bundle-url":"lgJ39"}],"i8izv":[function(require,module,exports) {
+module.exports = require("./helpers/bundle-url").getBundleURL("bLxZJ") + "rag.07b8cfc7.png" + "?" + Date.now();
 
 },{"./helpers/bundle-url":"lgJ39"}],"gXzeZ":[function(require,module,exports) {
 var $parcel$ReactRefreshHelpers$c34b = require("@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js");
